ae754c6ea3c536f5cdda8c74b437d81e59412ab6
Konstantin Petrov <kpetrov@ripe.net>
2020-04-03 11:17:05 +0200

    Add tree level parameter

--- src/main/scala/me/konstantinpetrov/convert/orgmode/OrgModeEntryTextFormatter.scala
+++ src/main/scala/me/konstantinpetrov/convert/orgmode/OrgModeEntryTextFormatter.scala
@@ -2,7 +2,9 @@ package me.konstantinpetrov.convert.orgmode
 
 import me.konstantinpetrov.convert.model.Entry
 
-class OrgModeEntryTextFormatter(maxWidth: Int = 50, wordWrap: Boolean = true) {
+class OrgModeEntryTextFormatter(level: Int = 4,
+                                maxWidth: Int = 50,
+                                wordWrap: Boolean = true) {
   def format(entry: Entry): String = {
     var candidatePosition = 0
     var lastNewLinePosition = 0
